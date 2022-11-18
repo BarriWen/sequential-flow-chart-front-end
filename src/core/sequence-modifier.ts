100e3ddb090d78f46027d5965c47dbf7107632ad
oliver-olive <65835019+oliver-olive@users.noreply.github.com>
2022-11-18 11:08:48 -0800

    Add files via upload

--- src/core/sequence-modifier.ts
+++ src/core/sequence-modifier.ts
@@ -23,11 +23,27 @@ export class SequenceModifier {
 		targetSequence.splice(targetIndex, 0, step);
 	}
 
-	public static deleteStep(step: Step, parentSequence: Sequence) {
-		const index = parentSequence.indexOf(step);
-		if (index < 0) {
-			throw new Error('Unknown step');
+	public static deleteStep(step: Step, parentSequence: Sequence, choice:string | number | null) {
+		if (choice == "0" && step.branches.False.length > 0){
+			console.log("delete true");
+			for (let i = 0; i < step.branches.False.length; i++) {
+				parentSequence.push(step.branches.False[i]);
+			}
+		} 
+		// If deleting false branch, keep blocks in true
+		else if (choice == "1" && step.branches.True.length > 0) {
+			console.log("delete false");
+			for (let i = 0; i < step.branches.True.length; i++) {
+				parentSequence.push(step.branches.True[i]);
+			}
+		} 
+		
+		if (choice != null) {
+			const index = parentSequence.indexOf(step);
+			if (index < 0) {
+				throw new Error('Unknown step');
+			}
+			parentSequence.splice(index, 1);
 		}
-		parentSequence.splice(index, 1);
 	}
 }
