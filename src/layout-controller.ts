7729d68551bc1244bf0896f4fd3ad60175aad901
oliver-olive <65835019+oliver-olive@users.noreply.github.com>
2022-11-18 11:24:08 -0800

    Update layout-controller.ts

--- src/layout-controller.ts
+++ src/layout-controller.ts
@@ -1,5 +1,5 @@
 export class LayoutController {
-	public constructor(private readonly parent: HTMLElement) {}
+	public constructor(public readonly parent: HTMLElement) {}
 
 	public isMobile(): boolean {
 		return this.parent.clientWidth < 400; // TODO
