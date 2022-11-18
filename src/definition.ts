29897ddfb89522d77a656fa6072b474caebcdd81
oliver-olive <65835019+oliver-olive@users.noreply.github.com>
2022-11-18 11:17:12 -0800

    Add files via upload

--- src/definition.ts
+++ src/definition.ts
@@ -1,42 +1,48 @@
 export interface Definition {
-	sequence: Sequence;
-	properties: Properties;
+  sequence: Sequence;
+  properties: Properties;
 }
 
 export type Sequence = Step[];
 
 export interface Step {
-	id: string;
-	componentType: ComponentType;
-	type: string;
-	name: string;
-	properties: Properties;
+  id: string;
+  componentType: ComponentType;
+  type: string;
+  name: string;
+  properties: Properties;
+  createdAt: Date;
+  createdBy: String;
+  updatedAt: Date;
+  updatedBy: string;
+  branches: any;
+  //branches: any;
 }
 
 export enum ComponentType {
-	task = 'task',
-	switch = 'switch',
-	container = 'container'
+  task = "task",
+  switch = "switch",
+  container = "container",
 }
 
 export interface TaskStep extends Step {
-	componentType: ComponentType.task;
+  componentType: ComponentType.task;
 }
 
 export interface SwitchStep extends Step {
-	componentType: ComponentType.switch;
-	branches: Branches;
+  componentType: ComponentType.switch;
+  branches: Branches;
 }
 
 export interface ContainerStep extends Step {
-	componentType: ComponentType.container;
-	sequence: Sequence;
+  componentType: ComponentType.container;
+  sequence: Sequence;
 }
 
 export interface Branches {
-	[branchName: string]: Sequence;
+  [branchName: string]: Sequence;
 }
 
 export interface Properties {
-	[name: string]: string | number;
+  [name: string]: string | number;
 }
