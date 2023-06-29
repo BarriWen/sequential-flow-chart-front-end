854fce84a47cdf7c0670e8cd9a3a1007f1b9fb87
BarriWen <wenzelongkyrie@outlook.com>
2023-06-29 03:10:23 -0400

    Change UI for blank options

--- src/workspace/task-step/test.js
+++ src/workspace/task-step/test.js
@@ -1,12 +1,10 @@
-if (choice1 == "Full Name" && /[^a-zA-Z0-9\s]/g.test(textInput.value)) {
-                    textInput.value = ""; // Reset
-                    return;
-                }
-                if ((choice1 == "First Name" || choice1 == "Last Name") && /[^a-zA-Z0-9]/g.test(textInput.value)) {
-                    textInput.value = ""; // Reset
-                    return;
-                }
-                if (choice1 == "Phone Number" && /[^0-9()-]/g.test(textInput.value)) {
-                    textInput.value = ""; // Reset
-                    return;
-                }
+const dropdownBoxBottomShape1 = Dom.svg("rect", {
+                    width: DROPDOWN2_W,
+                    height: list2.length * 25 + 10,
+                    fill: "#fff",
+                    stroke: "#247d99",
+                    x: DROPDOWN_X2,
+                    y: DROPDOWN_Y + DROPDOWN_H + 5,
+                    rx: 4,
+                    ry: 4
+                });
